17e38d083af1bf6166980dd528ed2813d1467969
Yash Amte <165381463+yashamte72@users.noreply.github.com>
2025-11-23 16:14:01 +0530

    Time: 2 ms (13.92%) | Memory: 17.8 MB (33.65%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            min_index = i
+
+            for j in range(i + 1, len(nums)):
+                if nums[j] < nums[min_index]:
+                    min_index = j
+            
+            nums[i], nums[min_index] = nums[min_index],nums[i]
+        return nums
+            
